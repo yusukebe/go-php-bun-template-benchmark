fae044c5b520da374c6e8017eaa972aefca8fa18
Yusuke Wada <yusuke@kamawada.com>
2024-06-20 00:04:33 +0900

    dont use json file (#1)

--- go-app/main.go
+++ go-app/main.go
@@ -1,32 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	tpl := template.Must(template.New("").ParseFiles("app.tpl"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		b, err := os.ReadFile("input.json")
-		if err != nil {
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		var data any
-		err = json.Unmarshal(b, &data)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		data := make([]string, 1000)
+			for i := 0; i < 1000; i++ {
+			data[i] = "ほげ"
 		}
 		fmt.Fprintln(w, "<html><body><ul>")
-		err = tpl.ExecuteTemplate(w, "index", data)
+		err := tpl.ExecuteTemplate(w, "index", data)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
